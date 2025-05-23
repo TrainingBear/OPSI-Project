193a367cd1cdd77c20237a52300049c863b8a5ab
KuJaTic <77871999+TrainingBear@users.noreply.github.com>
2025-05-23 16:28:25 +0700

    Add files via upload

---
+++ src/main/java/com/trbear9/openfarm/HelloController.java
@@ -0,0 +1,14 @@
+package com.trbear9.openfarm;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
